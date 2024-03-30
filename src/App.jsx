a69dbc6fb7f05f88e7aa18ded4580a7b71a138d4
thinhphungdev <phungthinh0946519356@gmail.com>
2024-03-30 22:14:14 +0700

    feat: structure section content  by components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,44 @@
+import {
+  CustomerReviews,
+  Footer,
+  Hero,
+  PopularProducts,
+  SpecialOffer,
+  Services,
+  Subscribe,
+  SuperQuality,
+} from './sections';
+
 export default function App() {
   return (
     <>
       <nav>Navbar</nav>
-      <header>Header</header>
-      <main>Main</main>
-      <footer>Footer</footer>
+      <header className='xl:padding-1 wide:padding-r padding-b'>
+        <Hero />
+      </header>
+      <main>
+        <section className='padding'>
+          <PopularProducts />
+        </section>
+        <section className='padding'>
+          <SuperQuality />
+        </section>
+        <section className='padding'>
+          <Services />
+        </section>
+        <section className='padding'>
+          <SpecialOffer />
+        </section>
+        <section className='padding bg-pale-blue'>
+          <CustomerReviews />
+        </section>
+        <section className='padding-x sm:px-32 py-16 w-full'>
+          <Subscribe />
+        </section>
+      </main>
+      <footer className='bg-black padding-x padding-t pb-8'>
+        <Footer />
+      </footer>
     </>
   );
 }
