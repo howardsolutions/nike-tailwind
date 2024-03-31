44da2792a29a469d7d26e8fc8519e4bb66b14785
thinhphungdev <phungthinh0946519356@gmail.com>
2024-03-31 16:03:55 +0700

    right side hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,23 +1,60 @@
+import { arrowRight } from '../assets/icons';
+import { bigShoe1 } from '../assets/images';
+import { Button } from '../components';
+import { shoes, statistics } from '../constants';
+import ShoeCard from '../components/ShoeCard';
 function Hero() {
   return (
-    <header id='home' className='xl:padding-1 wide:padding-r padding-b'>
-      <div className='w-full flex flex-col gap-10 xl:flex-row max-container min-h-screen'>
-        <div className='relative xl:w-2/5 flex justify-center max-xl:padding-x pt-28 items-start flex-col w-full'>
-          <p className='text-xl font-montserrat text-coral-red'>
-            Our Summer collections
-          </p>
+    <header
+      id='home'
+      className='w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container'
+    >
+      <div className='relative xl:w-2/5 flex justify-center max-xl:padding-x pt-28 items-start flex-col w-full'>
+        <p className='text-xl font-montserrat text-coral-red'>
+          Our Summer collections
+        </p>
 
-          <h1 className='mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold'>
-            <span className='xl:bg-white xl:whitespace-nowrap relative z-10 pr-10'>
-              The New Arrival
-            </span>
-            <br />
-            <span className='text-coral-red inline-block mt-3'>Nike</span> Shoes
-          </h1>
-          <p className='font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm'>
-            Discover stylish Nike arrivals, quality comfort, and innovation for
-            your active life.
-          </p>
+        <h1 className='mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold'>
+          <span className='xl:bg-white xl:whitespace-nowrap relative z-10 pr-10'>
+            The New Arrival
+          </span>
+          <br />
+          <span className='text-coral-red inline-block mt-3'>Nike</span> Shoes
+        </h1>
+        <p className='font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm'>
+          Discover stylish Nike arrivals, quality comfort, and innovation for
+          your active life.
+        </p>
+        <Button iconURL={arrowRight}>Shop Now</Button>
+        <div className='flex justify-start items-start gap-16 flex-wrap w-full mt-20'>
+          {statistics.map((statistic) => (
+            <div key={statistic.label}>
+              <p className='text-4xl font-bold font-palanquin'>
+                {statistic.value}
+              </p>
+              <p className='leading-7 font-montserrat text-slate-gray'>
+                {statistic.label}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className='relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-cover bg-primary bg-hero'>
+        <img
+          src={bigShoe1}
+          alt='Shoe Collection'
+          width={610}
+          height={500}
+          className='object-contain relative z-10'
+        />
+
+        <div>
+          {shoes.map((shoe) => (
+            <div key={shoe.thumbnail}>
+              <ShoeCard />
+            </div>
+          ))}
         </div>
       </div>
     </header>
